b0115c92927826a07546df0cbb0b8e1179946433
Winata Sukiman <m26415060@john.petra.ac.id>
2017-03-29 08:12:36 +0700

    list 1 done

--- PR5/list1.py
+++ PR5/list1.py
@@ -1,9 +1,4 @@
-# Basic list exercises
-# Fill in the code for the functions below. main() is already set up
-# to call the functions with a few different inputs,
-# printing 'OK' when each function is correct.
-# The starter code for each function includes a 'return'
-# which is just a placeholder for your code.
+
 # It's ok if you do not complete all the functions, and there
 # are some additional functions to try in list2.py.
 
